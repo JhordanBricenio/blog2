28675115ce0c6c454896039775ead22a87ec32dc
JhordanBricenio <jhordan795@hotmail.com>
2022-04-14 21:40:47 -0500

    Agregando el crud crear posts y error en el update

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
-use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\PostRequest;
 
 use Illuminate\Support\Facades\Storage;
 
@@ -45,7 +45,7 @@ class PostController extends Controller
         return view('admin.posts.create',compact('categories','tags'));
     }
 
-    public function store(StorePostRequest $request)
+    public function store(PostRequest $request)
     {
         //return Storage::put('posts',$request->file('file'));
         //Validando las reglas de validacion
@@ -74,13 +74,33 @@ class PostController extends Controller
 
     public function edit(Post $post)
     {
-        return view('admin.posts.edit' , compact('post'));
+        $categories=Category::pluck('name','id');
+        $tags=Tag::all();
+        return view('admin.posts.edit' , compact('post','categories','tags'));
     }
 
 
-    public function update(Request $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
-        //
+        $post->update($request->all());
+
+        if($request->file('file')){
+           $url= Storage::put('posts',$request->file('file'));
+           
+           if($post->image){
+               Storage::delete($post->image->url);
+               $post->image->update([
+                   'url'=> $url
+               ]);
+           }
+           else{
+               $post->image()->create([
+                   'url'=> $url
+               ]);
+           }
+        }
+
+        return redirect()->route('admin.posts.edit', $post)->with('info','El post se actualizó con éxito');
     }
 
     public function destroy(Post $post)
--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorePostRequest extends FormRequest
+class PostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,12 +13,9 @@ class StorePostRequest extends FormRequest
      */
     public function authorize()
     {
-        //limitar a usuarios 
-        if($this->user_id==auth()->user()->id){
+        //limitar a usuarios {
             return true;
-        }else{
-            return false;
-        }
+
     }
 
     /**
@@ -28,12 +25,18 @@ class StorePostRequest extends FormRequest
      */
     public function rules()
     {
+
+        $post = $this->route()->parameter('post');
+
         $rules= [
             'name'=>'required',
             'slug'=>'required|unique:posts',
             'status'=>'required|in:1,2'
 
         ];
+        if($post){
+            $rules['slug']= 'required|unique:posts,slug,' .$post->id;
+        }
         if($this->status==2){
             //Metodo de php paara agregar mas reglas encima dse las que ya estan 
             $rules=array_merge($rules,[
