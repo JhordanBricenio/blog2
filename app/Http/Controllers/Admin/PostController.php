709636552ff5be5b630fe4f4c20cd1ee68a5426f
JhordanBricenio <jhordan795@hotmail.com>
2022-04-14 21:46:06 -0500

    Solución Metodo update crud posts/ admin

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -52,7 +52,7 @@ class PostController extends Controller
         $post=Post::create($request->all());
         
         if($request->file('file')){
-            $url=Storage::put('posts',$request->file('file'));
+            $url=Storage::put('public/posts',$request->file('file'));
 
             $post->image()->create([
                 'url'=>$url
@@ -85,7 +85,7 @@ class PostController extends Controller
         $post->update($request->all());
 
         if($request->file('file')){
-           $url= Storage::put('posts',$request->file('file'));
+           $url= Storage::put('public/posts',$request->file('file'));
            
            if($post->image){
                Storage::delete($post->image->url);
